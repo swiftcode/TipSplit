2122e258229d7372cf1275e4aa7793ce95604cfe
swiftcode <mpc@junct.com>
2022-07-23 15:13:37 -0500

    Initial Commit

---
+++ TipSplit/TipSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSplitApp.swift
+//  TipSplit
+//
+//  Created by mpc on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct TipSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
